1c0a5013f48655c006f3e48cef46ea3df4cd41a6
Malwina <macza3623@oslomet.no>
2026-07-09 13:29:03 +0200

    adding Add Project form

--- frontend/src/pages/ProjectsPage.tsx
+++ frontend/src/pages/ProjectsPage.tsx
@@ -1,20 +1,75 @@
 import { useEffect, useState } from "react";
-import { getProjects } from "../api/project";
+import { getProjects, createProject } from "../api/project";
 import type { Project } from "../types/project";
+
 
 export default function ProjectsPage() {
     const [projects, setProjects] = useState<Project[]>([]);
 
+
+    const [projectNumber, setProjectNumber] = useState("");
+    const [projectName, setProjectName] = useState("");
+    const [customerName, setCustomerName] = useState("");
+    const [isActive, setIsActive] = useState(true);
+    
     useEffect(() => {
         getProjects()
             .then(setProjects)
             .catch(console.error);
     }, []);
 
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const newProject = await createProject({
+            projectNumber,
+            projectName,
+            customerName,
+            isActive,
+        });
+
+        setProjects([...projects, newProject]);
+
+        setProjectNumber("");
+        setProjectName("");
+        setCustomerName("");
+        setIsActive(true);
+    };
+
     return (
         <div>
             <h1>Projects</h1>
 
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    placeholder="Project number"
+                    value={projectNumber}
+                    onChange={(e) => setProjectNumber(e.target.value)} 
+                />
+                
+                <input type="text"
+                    placeholder="Project name"
+                    value={projectName}
+                    onChange={(e) => setProjectName(e.target.value)} 
+                />
+
+                <input type="text"
+                    placeholder="Customer name"
+                    value={customerName}
+                    onChange={(e) => setCustomerName(e.target.value)} 
+                />
+
+                <label>
+                    <input type="checkbox"
+                    checked={isActive}    
+                    onChange={(e) => setIsActive(e.target.checked)} 
+                />
+                Active
+                </label>
+
+                <button type="submit">Add Project</button>
+            </form>
+            
             {projects.length === 0 ? (
                 <p>No projects found</p>
             ) : (
