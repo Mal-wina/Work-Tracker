c62c6c2ddfad525b46779777263b3d336ee24543
Malwina <macza3623@oslomet.no>
2026-07-10 13:48:02 +0200

    created navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
 import ProjectsPage from "./pages/ProjectsPage";
+import UsersPage from "./pages/UsersPage";
+import WorkEntriesPage from "./pages/WorkEntriesPage";
 
 function App() {
-  return <ProjectsPage />;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Dashboard</Link>{" "}
+        <Link to="/projects">Projects</Link>{" "}
+        <Link to="/users">Employees</Link>{" "}
+        <Link to="/workentries">Work Entries</Link>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/workentries" element={<WorkEntriesPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
